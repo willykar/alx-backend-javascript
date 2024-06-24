89d31736ec90d026a4432e32379423fbdeae7443
willykar <103494595+willykar@users.noreply.github.com>
2024-06-25 00:43:05 +0300

    Update 0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,17 @@
+// 0-constants.js
+
 export function taskFirst() {
-	const task = 'I prefer const when I can.';
-	return task;
-  }
+  const task = 'I prefer const when I can.';
+  return task;
+}
 
-  export function getLast() {
-	return ' is okay';
-  }
+export function getLast() {
+  return ' is okay';
+}
 
-  export function taskNext() {
-	let combination = 'But sometimes let';
-	combination += getLast();
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
 
-	return combination;
-  }
+  return combination;
+}
