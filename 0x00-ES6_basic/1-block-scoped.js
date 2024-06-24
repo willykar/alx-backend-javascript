2dc8df407ba828935d72fe325189cb51593dda22
willykar <103494595+willykar@users.noreply.github.com>
2024-06-25 01:21:49 +0300

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,12 @@
-// 1-block-scoped.js
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-var */
 
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    // eslint-disable-next-line no-unused-vars, no-shadow
     const task = true;
-    // eslint-disable-next-line no-unused-vars, no-shadow
     const task2 = false;
   }
 
