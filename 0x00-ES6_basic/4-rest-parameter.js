8972a802ee932030676174e65eb9fd7826bc9f34
willykar <103494595+willykar@users.noreply.github.com>
2024-06-25 00:10:13 +0300

    Update 4-rest-parameter.js

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,8 +1,8 @@
 /**
- * Calculates the number of arguments passed to the function.
+ * Calculates the number of arguments passed to the function
  *
- * @param {array} args - The arguments passed to the function.
- * @return {number} The number of arguments passed.
+ * @param {array} args - The arguments passed to the function(variable number)
+ * @return {number} The number of arguments passed
  */
 export default function returnHowManyArguments(...args) {
 	return args.length;
