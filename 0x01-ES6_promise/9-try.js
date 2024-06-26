1fd440fd1ae850373005c1c43e09464b20b08dee
willykar <wilsonnjenga799@gmail.com>
2024-06-26 15:43:46 -0700

    9-try

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -2,9 +2,11 @@ export default function guardrail(mathFunction) {
   const queue = [];
   try {
     const result = mathFunction();
-    queue.push(result, 'Guardrail was processed');
+    queue.push(result);
   } catch (err) {
     queue.push(err.toString(), 'Guardrail was processed');
+  } finally {
+	  queue.push('Guardrail was processed');
   }
   return queue;
 }
