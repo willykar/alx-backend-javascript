74f9eabf88f5cd85d17652ce8127807265b05f92
willykar <wilsonnjenga799@gmail.com>
2024-06-26 16:36:20 -0700

    0-promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,4 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    resolve('An Example implementation');
-    reject(new Error('Error message here'));
   });
 }
