b301867e74285a399e94e50b28731dfb45af034f
willykar <wilsonnjenga799@gmail.com>
2024-06-26 16:04:39 -0700

    100-await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,19 +1,20 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function asyncUploadUser(){
-	try {
-		const [photo, user] = await Promise.all([
-			uploadPhoto(),
-			createUser(),
-		]);
-		return {
-			photo: photo,
-			user: user,
-		};
-	} catch (err) {
-		return {
-			photo: null,
-			user: null,
-		};
-	}
+export default async function asyncUploadUser() {
+  try {
+    const [photo, user] = await Promise.all([
+      uploadPhoto(),
+      createUser()
+    ]);
+
+    return {
+      photo: photo,
+      user: user
+    };
+  } catch (err) {
+    return {
+      photo: null,
+      user: null
+    };
+  }
 }
