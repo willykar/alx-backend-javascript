4b4378fbe2be21a8ae806761d56c0f46b7e616f7
willykar <wilsonnjenga799@gmail.com>
2024-06-26 15:12:53 -0700

    6-final-user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,22 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default handleProfileSignup(firstName, lastName, fileName){
-	const [user, photo] = await Promise.all([
-		signUpUser(firstName, lastName),
-		uploadPhoto(fileName)]);
-	return [{ status: 'fulfilled', value: user },
-		{ status: 'fulfilled', value: photo}];
-	catch (error) {
-		return [
-			{status: 'rejected', value: error }]
-	}
+async function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const [user, photo] = await Promise.all([
+      signUpUser(firstName, lastName),
+      uploadPhoto(fileName)
+    ]);
+
+    return [
+      { status: 'fulfilled', value: user },
+      { status: 'fulfilled', value: photo }
+    ];
+  } catch (error) {
+    return [
+      { status: 'rejected', value: error }
+    ];
+  }
 }
+
+export default handleProfileSignup;
