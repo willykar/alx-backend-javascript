5a7fef447ae36774384c753c2d156a147c74b232
willykar <wilsonnjenga799@gmail.com>
2024-06-26 13:08:14 -0700

    1-promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+export default function getFullResponseFromAPI(success) {
+  const prom = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+  return prom;
+}
