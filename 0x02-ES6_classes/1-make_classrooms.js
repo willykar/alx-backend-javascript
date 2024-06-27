79d0526d18e23fa49c5de74c956bec95fa88e051
willykar <wilsonnjenga799@gmail.com>
2024-06-27 12:03:22 -0700

    1-make_classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,5 @@
-import ClassRoom from './0-classroom.js'
+import ClassRoom from './0-classroom';
 
-function initializeRooms(){
-	return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+export default function initializeRooms() {
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
