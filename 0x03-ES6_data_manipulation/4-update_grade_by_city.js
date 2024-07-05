331e437db375eeda63b8727f559439245b735b75
willykar <wilsonnjenga799@gmail.com>
2024-07-05 11:29:41 -0700

    4-update_grade_by_city

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,31 @@
-import getListStudents from "./0-get_list_students.js";
-
 export default function updateStudentGradeByCity(students, city, newGrades) {
-    return students
-        .filter(student => student.location === city)
-        .map(student => {
-            const foundGrade = newGrades.find(grade => grade.studentId === student.id);
-            if (foundGrade) {
-                return { ...student, grade: foundGrade.grade };
-            } else {
-                return { ...student, grade: 'N/A' };
-            }
-        });
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  if (!Array.isArray(newGrades)) {
+    return [];
+  }
+
+  const stCity = students.filter((student) => student.location === city);
+
+  const studentsGraded = stCity.map((student) => {
+    const gradeFilter = newGrades.filter(
+      (newGrade) => newGrade.studentId === student.id,
+    );
+
+    let grade;
+
+    if (gradeFilter[0]) {
+      grade = gradeFilter[0].grade;
+    } else {
+      grade = 'N/A';
+    }
+
+    return {
+      ...student,
+      grade,
+    };
+  });
+
+  return studentsGraded;
 }
