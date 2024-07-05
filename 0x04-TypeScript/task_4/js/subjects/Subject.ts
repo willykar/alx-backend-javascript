ca8ad8683d4efb8c6c202076186c97ed0d7924bc
willykar <wilsonnjenga799@gmail.com>
2024-07-05 13:37:13 -0700

    typescript task_4

---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,15 @@
+// Subject.ts
+
+namespace Subjects {
+    export class Subject {
+        private _teacher: Teacher | null = null;
+
+        public setTeacher(teacher: Teacher): void {
+            this._teacher = teacher;
+        }
+
+        get teacher(): Teacher | null {
+            return this._teacher;
+        }
+    }
+}
---
+++ 0x04-TypeScript/task_4/js/subjects/Teacher.ts
@@ -0,0 +1,11 @@
+// Teacher.ts
+
+namespace Subjects {
+    export interface Teacher {
+        firstName: string;
+        lastName: string;
+        experienceTeachingC?: number;
+        experienceTeachingReact?: number;
+        experienceTeachingJava?: number;
+    }
+}
