641db840d2f60838015553e9fae33834d4a1c428
willykar <103494595+willykar@users.noreply.github.com>
2024-08-22 23:46:08 +0300

    Create 1-calcul.test.js

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert')
+const calculateNumber = require('./1-calcul')
+
+describe('calculateNumber', function() {
+        it('should return 6 for SUM with inputs 1.4 and 4.5', function() {
+                assert.StrictEqual(calculateNumber('SUM', 1.4, 4.5), 6)
+        })
+        it('should return -4 for SUBTRACT with inputs 1.4 and 4.5', function() {
+                assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+        });
+        it('should return 0.2 for DIVIDE with inputs 1.4 and 4.5', function() {
+                assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+        });
+        it('should return "Error" for DIVIDE with inputs 1.4 and 0', function() {
+                assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+        });
+});
