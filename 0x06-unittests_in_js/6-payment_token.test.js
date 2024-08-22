0106022466b37e6724b4ec94ee8383b424a44e94
willykar <103494595+willykar@users.noreply.github.com>
2024-08-23 00:30:41 +0300

    Create  6-payment_token.test.js

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,12 @@
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token.js');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('should return a resolved promise with successful response when success is true', (done) => {
+  getPaymentTokenFromAPI(true)
+    .then((response) => {
+      expect(response).to.deep.equal({data: 'Successful response from the API' });
+      done();
+    });
+  });
+});
