d139c89544951a01f76d5f499fbc69ec5d61a2ef
willykar <103494595+willykar@users.noreply.github.com>
2024-08-23 00:24:40 +0300

    Create 4-payment.test.js

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,19 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils.js');
+const sendPaymentRequestToApi = require('./4-payment.js');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utils.calculateNumber with type SUM and return 10', () => {
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const consoleSpy = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(consoleSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+
+    calculateNumberStub.restore();
+    consoleSpy.restore();
+  });
+});
