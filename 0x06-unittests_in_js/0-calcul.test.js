19566f89645ed61f5b835fea59ac477a4ae30021
willykar <103494595+willykar@users.noreply.github.com>
2024-08-23 00:58:07 +0300

    Update 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,17 +1,16 @@
-const assert = require('assert')
-const calculateNumber = require('./0-calcul')
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function() {
-        it('should return 4 when inputs are 1 and 3', function() {
-                assert.strictEqual(calculateNumber(1, 3), 4)
-        })      
-        it('should return 5 when inputs are 1 and 3.7', function() {
-                assert.strictEqual(calculateNumber(1, 3.7), 5)
-        })      
-        it('should return 5 when inputs are 1.2 and 3.7', function() {
-                assert.strictEqual(calculateNumber(1.2, 3.7), 5)
-	})
-	it('should return 6 when inputs are 1.5 and 3.7', function() {
-		assert.strictEqual(calculateNumber(1.5, 3.7), 6)
-	})
-})
+describe('calculateNumber', () => {
+  it('should return the sum of rounded numbers', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(-1, -3), -4);
+    assert.strictEqual(calculateNumber(-1, 3.7), 3);
+    assert.strictEqual(calculateNumber(1.5, -3.7), -2);
+    assert.strictEqual(calculateNumber(1, -3), -2);
+    assert.strictEqual(calculateNumber(-1, -3.7), -5);
+  });
+});
