e3bb99ebf0df8f0d2e3848bd537c82b957c1d832
willykar <103494595+willykar@users.noreply.github.com>
2024-08-23 00:22:50 +0300

    Create 3-payment.test.js

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,15 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils.js');
+const sendPaymentRequestToApi = require('./3-payment.js');
+
+describe('sendPaymentRequestToApi', function() {
+  it('should call Utils.calculateNumber with type SUM', () => {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberSpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    calculateNumberSpy.restore();
+  });
+});
