d8da1ffe2ce9a8b85f3d5b032070aa8318a9de24
willykar <103494595+willykar@users.noreply.github.com>
2024-08-23 00:13:28 +0300

    Update 1-calcul.test.js

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,17 +1,30 @@
-const assert = require('assert')
-const calculateNumber = require('./1-calcul')
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
 
-describe('calculateNumber', function() {
-        it('should return 6 for SUM with inputs 1.4 and 4.5', function() {
-                assert.StrictEqual(calculateNumber('SUM', 1.4, 4.5), 6)
-        })
-        it('should return -4 for SUBTRACT with inputs 1.4 and 4.5', function() {
-                assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
-        });
-        it('should return 0.2 for DIVIDE with inputs 1.4 and 4.5', function() {
-                assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
-        });
-        it('should return "Error" for DIVIDE with inputs 1.4 and 0', function() {
-                assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
-        });
+describe('calculateNumber', () => {
+  describe('type SUM', () => {
+    it('should return the sum of rounded numbers', () => {
+      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+      assert.strictEqual(calculateNumber('SUM', -2, -5.9), -8);
+    });
+  });
+
+  describe('type SUBTRACT', () => {
+    it('should return the subtraction of rounded numbers', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+      assert.strictEqual(calculateNumber('SUBTRACT', -2, -5.9), 4);
+    });
+  });
+
+  describe('type DIVIDE', () => {
+    it('should return the division of rounded numbers', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+      assert.strictEqual(calculateNumber('DIVIDE', -2, -5.9), (2 / 6));
+    });
+
+    it('should return Error when divisor is zero', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+      assert.strictEqual(calculateNumber('DIVIDE', -2, 0), 'Error');
+    });
+  });
 });
