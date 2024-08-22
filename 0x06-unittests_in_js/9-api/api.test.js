da549924478f145687a979cc51907df4425c0ee3
willykar <103494595+willykar@users.noreply.github.com>
2024-08-23 00:49:47 +0300

    Create regex api.test.js

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,29 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', () => {
+  it('should return correct status code and message', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart page', () => {
+  it('should return correct status code when :id is a number', (done) => {
+    request('http://localhost:7865/cart/12', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('should return 404 status code when :id is NOT a number', (done) => {
+    request('http://localhost:7865/cart/hello', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
