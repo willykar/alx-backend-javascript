003438bc5f03b2d2f2e377a68a731612dbf9ec8d
willykar <103494595+willykar@users.noreply.github.com>
2024-08-23 00:32:25 +0300

    Update 6-payment_token.test.js

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token.js');
 
-describe('getPaymentTokenFromAPI', () => {
+describe('getPaymentTokenFromAPI', function() {
   it('should return a resolved promise with successful response when success is true', (done) => {
   getPaymentTokenFromAPI(true)
     .then((response) => {
